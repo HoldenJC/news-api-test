1b789a4d998ac19ec69009cda9a09935d3f7a004
HoldenJC <holdenj_c@yahoo.com>
2019-08-20 17:53:23 -0700

    add project following along with C# week 6 monday night API HW

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApiCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            var client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            // 2
+            var request = new RestRequest("home.json?api-key=xNdLOT46LfLS2I1cIOv5IVHXqaF2vdDE", Method.GET);
+            // 3
+            var response = new RestResponse();
+
+            // 4
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            Console.WriteLine(jsonResponse["results"]);
+        }
+
+        // 5
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
